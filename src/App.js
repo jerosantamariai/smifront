eebf7ccadac29af674e11f6b45a62d3c63430334
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-15 22:26:11 -0400

    adding a basic and not finished navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import injectContext from './store/appContext';
 import Home from './views/home';
 import NotFound from './views/notfound';
+import Navbar from './components/navbar';
 
 const App = props => {
   return (
-     <BrowserRouter>
-         <Switch>
-             <Route exact path="/" component={Home} />
-             <Route component={NotFound} />
-         </Switch>
-     </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 

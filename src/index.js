9d231c25e34a42ebf81e43267e6bea14b4c3c2aa
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-15 21:59:35 -0400

    libraries installed and configurated... bootstrap jquery@4.3.1 fontawesome and popper

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
